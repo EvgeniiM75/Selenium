c0082b6d557dbf597fed4b0f61a28f83344c1757
Machete <75machet@gmail.com>
2025-04-20 14:06:08 +0300

    tests ready2

--- conftest.py
+++ conftest.py
@@ -5,10 +5,10 @@ import pytest
 
 @pytest.fixture()
 def driver():
-    #options = Options()                      # Для невидимого запуска браузера
-    #options.add_argument('--headless')       #  Для невидимого запуска браузера
-    #driver=webdriver.Chrome(options=options) # Для невидимого запуска браузера
-    driver = webdriver.Chrome()               # Предусловие
+    options = Options()                      # Для невидимого запуска браузера
+    options.add_argument('--headless')       #  Для невидимого запуска браузера
+    driver=webdriver.Chrome(options=options) # Для невидимого запуска браузера
+    #driver = webdriver.Chrome()               # Предусловие
     driver.maximize_window()
     driver.implicitly_wait(3) # Ждем загрузки элементов
     yield driver
